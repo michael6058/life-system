85da9858faf7eb26efd526c503e6fad6f5771c74
michael6058 <michael6058@gmail.com>
2026-03-10 22:02:28 +0800

    Update locations.js

--- js/locations.js
+++ js/locations.js
@@ -1,10 +1,19 @@
+let items=[];
+
 fetch("data/inventory.json")
 .then(r=>r.json())
 .then(data=>{
 
+items=data;
+showLocations(items);
+
+});
+
+function showLocations(list){
+
 let locations={};
 
-data.forEach(item=>{
+list.forEach(item=>{
 
 if(!locations[item.location]){
 locations[item.location]=[];
@@ -29,5 +38,22 @@ html+="</ul>";
 }
 
 document.getElementById("locations").innerHTML=html;
+
+}
+
+document.getElementById("search").addEventListener("input",function(){
+
+let keyword=this.value.toLowerCase().trim();
+
+if(keyword===""){
+document.getElementById("locations").innerHTML="";
+return;
+}
+
+let filtered=items.filter(i=>
+i.location.toLowerCase().includes(keyword)
+);
+
+showLocations(filtered);
 
 });
