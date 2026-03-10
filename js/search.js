8d7d60737d7ca6ed45bc62b9f020ea245c185ea3
michael6058 <michael6058@gmail.com>
2026-03-10 18:49:19 +0800

    Update search.js

--- js/search.js
+++ js/search.js
@@ -6,10 +6,8 @@ fetch("data/recipes.json").then(r=>r.json()),
 fetch("data/inventory.json").then(r=>r.json())
 ])
 .then(data=>{
-
 recipes=data[0];
 items=data[1];
-
 });
 
 document.getElementById("search").addEventListener("input",function(){
@@ -23,9 +21,14 @@ return;
 
 let html="";
 
+/* 搜尋料理 */
+
 recipes.forEach(r=>{
 
-if(r.name.toLowerCase().includes(keyword)){
+if(
+r.name.toLowerCase().includes(keyword) ||
+r.ingredients.join("").toLowerCase().includes(keyword)
+){
 
 html+=`
 <h3>料理：${r.name}</h3>
@@ -46,6 +49,8 @@ ${r.steps.map(s=>`<li>${s}</li>`).join("")}
 }
 
 });
+
+/* 搜尋物品 */
 
 items.forEach(i=>{
 
@@ -57,53 +62,6 @@ html+=`
 位置：${i.location}<br>
 數量：${i.quantity}
 
-<hr>
-`;
-
-}
-
-});
-
-document.getElementById("results").innerHTML=html;
-
-});
-
-let keyword=this.value.toLowerCase();
-
-let html="";
-
-recipes.forEach(r=>{
-
-if(r.name.toLowerCase().includes(keyword)){
-
-html+=`
-<h3>料理：${r.name}</h3>
-
-<b>食材</b>
-<ul>
-${r.ingredients.map(i=>`<li>${i}</li>`).join("")}
-</ul>
-
-<b>步驟</b>
-<ol>
-${r.steps.map(s=>`<li>${s}</li>`).join("")}
-</ol>
-
-<hr>
-`;
-
-}
-
-});
-items.forEach(i=>{
-
-if(i.name.toLowerCase().includes(keyword)){
-
-html+=`
-<h3>物品</h3>
-${i.name}<br>
-位置：${i.location}<br>
-數量：${i.quantity}
 <hr>
 `;
 
